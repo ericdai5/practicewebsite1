5b09190a4baa0d958ee7c5b431dd2632c76ccf83
Eric Dai <ericdai2000@gmail.com>
2022-09-23 13:59:39 -0400

    first commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,19 +9,19 @@ const Footer = () => {
     <footer>
       <a href="#" className='footer__logo'>Eric Dai</a>
       <ul className='permalinks'>
-        <li><a hfref="#">Home</a></li>
-        <li><a hfref="#about">About</a></li>
-        <li><a hfref="#experience">Experience</a></li>
-        <li><a hfref="#services">Services</a></li>
-        <li><a hfref="#portfolio">Portfolio</a></li>
-        <li><a hfref="#testimonials">Testimonials</a></li>
-        <li><a hfref="#contact">Contact</a></li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
 
       <div className="footer__socials">
-        <a hfref="https://facebook.com"><FaFacebookF/></a>
-        <a hfref="https://instagram.com"><FiInstagram/></a>
-        <a hfref="https://twitter.com"><IoLogoTwitter/></a>
+        <a href="https://facebook.com"><FaFacebookF/></a>
+        <a href="https://instagram.com"><FiInstagram/></a>
+        <a href="https://twitter.com"><IoLogoTwitter/></a>
       </div>
       
       <div className="footer__copyright">
